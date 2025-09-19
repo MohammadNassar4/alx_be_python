09ecb47e57e2a418455fa044a97636850f5c9e1d
mohammadnassar4 <mohammadnassar044@gmail.com>
2025-09-20 01:46:39 +0300

    A script that take two numbers and an operator from the user and preform the operation on the numbers.

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ")
+
+match operator:
+    case "+":
+        print(num1 + num2)
+    case "-":
+        print(num1 - num2)
+    case "*":
+        print(num1 * num2)
+    case "/":
+        if num1 == 0 or num2 == 0:
+            print("Can't devide by Zero.")
+        else:
+            print(num1 / num2)
