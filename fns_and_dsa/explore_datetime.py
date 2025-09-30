2c185a26a0ac5009403f58df0d99ff221fd61b5e
mohammadnassar4 <mohammadnassar044@gmail.com>
2025-09-30 17:15:34 +0300

    Modifies the way i formated the current date and time.

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,10 +1,12 @@
 import datetime
 from datetime import timedelta
+import time
 
 current_date = datetime.datetime.now()
 
 def display_current_datetime():
-    print(f"Current date and time: {current_date.year}-{current_date.month}-{current_date.day} {current_date.hour}:{current_date.minute}:{current_date.second}")
+    curr_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print(curr_date)
 
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
