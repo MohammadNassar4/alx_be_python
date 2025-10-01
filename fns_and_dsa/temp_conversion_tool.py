7091d98e473668c7e5d0deac6698f0252f8cde19
mohammadnassar4 <mohammadnassar044@gmail.com>
2025-10-01 04:24:17 +0300

    Created a script that converts from celsius to fahrenheite and vise versa.

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,30 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    if celsius == 0:
+        return 32.0
+    else:
+        return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+
+def main():
+    temperature = float(input("Enter the temperature to convert: "))
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+
+    try:
+        val = float(temperature)
+        if unit == "F":
+            result = convert_to_celsius(temperature)
+            print(f"{temperature}°F is {result}°C")
+        elif unit == "C":
+            result = convert_to_fahrenheit(temperature)
+            print(f"{temperature}°C is {result}°F")
+        else:
+            print("Invalid unit")
+    except:
+        print("Invalid temperature. Please enter a numeric value.")
+
+main()
